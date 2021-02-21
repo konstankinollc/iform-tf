a2331bb4249540d669d67e05b0f025ffb7bebb12
Bogdan Kulbida <info@konstankino.com>
2021-02-21 10:09:42 +0200

    Say YES to updates.

--- user-data/app-server.sh
+++ user-data/app-server.sh
@@ -3,7 +3,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 mkdir -p /var/log/nginx/
 
-yum update
+yum -y update
 yum -y install libpng12 libpng12.so.0
 
 rm -fr /srv/iform/app/*
